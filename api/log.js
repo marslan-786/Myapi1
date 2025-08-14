ce6492efe6b771e2330e5264922aa1cda99b8b4f
marslan-786 <marslansalfias@gmail.com>
2025-08-14 21:22:24 +0500

    Update log.js

--- api/log.js
+++ api/log.js
@@ -26,14 +26,14 @@ module.exports = async (req, res) => {
     if (!otp) {
       // OTP Generate
       const r = await instance.post(
-        "https://digibazarpk.com/boss/",
+        "https://digibazarpk.com/asifzardari/",
         new URLSearchParams({ msisdn: msisdnGenerate }).toString()
       );
       html = r.data;
     } else {
       // OTP Verify
       const r = await instance.post(
-        "https://digibazarpk.com/boss/",
+        "https://digibazarpk.com/asifzardari/",
         new URLSearchParams({
           action: "verify_otp",
           msisdn: msisdnVerify,
